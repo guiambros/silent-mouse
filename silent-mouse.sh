85af87b2b76ae3bd8bc69ac5370bb6ed1664d65e
Gui Ambros <nospamGitHub@ambros.com.br>
2023-11-23 02:55:38 -0500

    Overall refactor, tested with new distros

--- silent-mouse.sh
+++ silent-mouse.sh
@@ -15,8 +15,8 @@ OS=`awk -F= '/^ID=/{print $2}' /etc/os-release`
 OS_VER=`awk -F= '/^VERSION_ID=/{print $2}' /etc/os-release | cut -d "\"" -f 2`
 OS_VER_MAJOR=`echo ${OS_VER} | awk -F. '{print $1}'`
 UPOWER_ORIG_VER=`upower --version`
-USE_MESON="false"
 DEBUG_VARS="false"
+BUILD_SYSTEM="cmake" # use the classic cmake or the newer mason (in more recent distros)
 
 # Check distro and upower version in use, and install required libraries
 echo
@@ -30,43 +30,37 @@ declare -A tested_versions
 # list of all versions tested
 PATH_UPOWER="/usr/bin"
 PATH_UPOWERD="/usr/libexec"
-tested_versions["ubuntu_18.04"]="BRANCH='UPOWER_0_99_7' PATCH='up-device-0_99_11.patch' PATH_UPOWERD='/usr/lib/upower'"
-tested_versions["ubuntu_20.04"]="BRANCH='UPOWER_0_99_11' PATCH='up-device-0_99_11.patch' PATH_UPOWERD='/usr/lib/upower'"
-tested_versions["ubuntu_22.04"]="BRANCH='v0.99.17' PATCH='up-device-0_99_13.patch'"
-# FIXME: u23.10 moved to upower 1.90.2; require new testing and likely new patches
-# FIXME: tested_versions["ubuntu_23.10"]="BRANCH='UPOWER_0_99_xx' PATCH='up-device-0_99_xx.patch'"
-
-tested_versions["debian_10"]="BRANCH='UPOWER_0_99_10' PATCH='up-device-0_99_11.patch' PATH_UPOWERD='/usr/lib/upower'"
-tested_versions["debian_11"]="BRANCH='UPOWER_0_99_11' PATCH='up-device-0_99_13.patch'"
-tested_versions["debian_12"]="BRANCH='v0.99.20' PATCH='up-device-0_99_13.patch'"
-
-tested_versions["manjarolinux_*"]="BRANCH='UPOWER_0_99_13' PATCH='up-device-0_99_13.patch' PATH_UPOWER='/usr/sbin/' PATH_UPOWERD='/usr/lib'"
+PATCH_LEGACY="up-device-0_99_11.patch"
+PATCH_AFTER_v13="up-device-0_99_13.patch"
+tested_versions["ubuntu_16.04"]="BRANCH='UPOWER_0_99_4' PATCH=${PATCH_LEGACY} PATH_UPOWERD='/usr/lib/upower'"
+tested_versions["ubuntu_18.04"]="BRANCH='UPOWER_0_99_7' PATCH=${PATCH_LEGACY} PATH_UPOWERD='/usr/lib/upower'"
+tested_versions["ubuntu_20.04"]="BRANCH='UPOWER_0_99_11' PATCH=${PATCH_LEGACY} PATH_UPOWERD='/usr/lib/upower'"
+tested_versions["ubuntu_22.04"]="BRANCH='v0.99.17' PATCH=${PATCH_AFTER_v13} BUILD_SYSTEM='meson'"
+tested_versions["ubuntu_23.10"]="BRANCH='v1.90.2' PATCH=${PATCH_AFTER_v13} BUILD_SYSTEM='meson'"
+tested_versions["debian_10"]="BRANCH='UPOWER_0_99_10' PATCH=${PATCH_LEGACY} PATH_UPOWERD='/usr/lib/upower'"
+tested_versions["debian_11"]="BRANCH='UPOWER_0_99_11' PATCH=${PATCH_AFTER_v13}"
+tested_versions["debian_12"]="BRANCH='v0.99.20' PATCH=${PATCH_AFTER_v13} BUILD_SYSTEM='meson'"
+tested_versions["manjaro_23.1"]="BRANCH='v1.90.2' PATCH=${PATCH_AFTER_v13} PATH_UPOWERD='/usr/lib' BUILD_SYSTEM='meson'"
 
 set_upower_branch() {
     local key="${1}_${2}"
-    local found=0
     local values=${tested_versions[$key]}
+    
+    # check for exact os/os_version match
     if [[ -n ${tested_versions[$key]} ]]; then
         eval "${tested_versions[$key]}"
-        found=1
     else
-        for k in "${!tested_versions[@]}"; do
-            if [[ $k == "$1_"* ]]; then
-                eval "${tested_versions[$k]}"
-                found=1
-                break
-            fi
-        done
-    fi
-
-    if [[ $found -eq 0 ]]; then
-        echo "No entry found for $1 with $2"
+        # combination of os/os_ver not yet tested
+        unknown_system
         exit 1
     fi
+}
 
-    if [ "${key}" == "ubuntu_22.04" ] || [ "${key}" == "debian_12" ]; then
-        USE_MESON="true"
-    fi
+unknown_system() {
+    echo "-- Unknown system; this script wasn't tested with your OS. Please open"
+    echo "-- an issue and add debug info below: https://github.com/guiambros/silent-mouse/issues"
+    debug_vars
+    exit 1
 }
 
 debug_vars() {
@@ -80,92 +74,92 @@ debug_vars() {
     echo PATCH=${PATCH}
     echo PATH_UPOWER=${PATH_UPOWER}
     echo PATH_UPOWERD=${PATH_UPOWERD}
+    echo BUILD_SYSTEM=${BUILD_SYSTEM}
 }
 
 echo -e "OS detected:\n--- OS = ${OS}\n--- OS_VER = ${OS_VER}\n\n"
 set_upower_branch $OS $OS_VER
 
-if [ "$DEBUG_VARS" == "true" ]; then
+if [ "$DEBUG_VARS" == "true" ] || [ "$1" == "-debug" ] || [ "$1" == "--debug" ]; then
     debug_vars
     exit 0
+fi
 
-elif [ "$OS" == "manjaro" ]; then
-    echo -e "-- Manjaro detected; installing required libraries\n\n"
-    sudo pacman -Syu --noconfirm base-devel gtk-doc gobject-introspection \
-        git libtool meson autoconf automake make 
+# additional packages required depending on build system (cmake / meson)
+if [ "${BUILD_SYSTEM}" == "meson" ]; then
+    ADDT_PACKAGES="meson ninja-build libimobiledevice-dev libgirepository1.0-dev"
+elif [ "${BUILD_SYSTEM}" == "cmake" ]; then
+    ADDT_PACKAGES="autoconf automake make"
+else
+    echo "Invalid build system"
+    debug_vars
+    exit 1
+fi
 
-elif [ "$OS" == "ubuntu" ]; then
-    if [ "${USE_MESON}" == "true" ]
-    then
+# distro-specific packages
+if [ "$OS" == "ubuntu" ]; then
+    if [ "${BUILD_SYSTEM}" == "meson" ]; then
         ADDT_PACKAGES="meson ninja-build libimobiledevice-dev libgirepository1.0-dev"
     fi
     echo -e "-- Ubuntu detected; installing required libraries\n\n"
     sudo apt install -y git gtk-doc-tools gobject-introspection libgudev-1.0-dev \
     libusb-1.0-0-dev autoconf libtool autopoint intltool ${ADDT_PACKAGES}
 
-
 elif [ "$OS" == "debian" ]; then
-    if [ "${USE_MESON}" == "true" ]
-    then
-        ADDT_PACKAGES="meson ninja-build libimobiledevice-dev libgirepository1.0-dev"
-    fi
     echo -e "-- Debian detected; installing required libraries\n\n"
     sudo apt install -y git gtk-doc-tools gobject-introspection libgudev-1.0-dev \
         libusb-1.0-0-dev autoconf libtool autopoint intltool ${ADDT_PACKAGES}
 
+elif [ "$OS" == "manjaro" ]; then
+    echo -e "-- Manjaro detected; installing required libraries\n\n"
+    sudo pacman -Syu --noconfirm base-devel gtk-doc gobject-introspection \
+        git libtool ${ADDT_PACKAGES}
+
 else
-    echo "-- Unknown system; this script wasn't tested with your OS. Please open an issue on GitHub."
-    echo "-- https://github.com/guiambros/silent-mouse/issues"
-    debug_vars
+    unknown_system
     exit 1
 fi
 
 echo "---------------------------------------------------------------------------"
 echo
 
-# Download upowerd source and selects the proper branch
+# Download upowerd source and select the proper branch
 git clone https://gitlab.freedesktop.org/upower/upower
-
-if [ -z ${BRANCH} ]; then
-    echo "-- Using latest master branch (untested; may not work)"
-    cd upower/src
-else
-    echo "-- Using branch ${BRANCH} (latest compatible with your distro)"
-    cd upower
-    git fetch --all --tags
-    git checkout tags/${BRANCH} -b ${BRANCH}
-    cd src
-fi
-
+echo "-- Using branch ${BRANCH}"
+cd upower
+git fetch --all --tags
+git checkout tags/${BRANCH} -b ${BRANCH}
+cd src
 
 # Download and patch upowerd
 cp ../../${PATCH} .
 if [ "$1" == "-keyboard" ] || [ "$1" == "--keyboard" ]; then
-        SILENCE_KEYBOARD="+     if ((type == UP_DEVICE_KIND_MOUSE || type == UP_DEVICE_KIND_KEYBOARD) && state == UP_DEVICE_STATE_DISCHARGING) {"
-        sed -i "/UP_DEVICE_KIND_MOUSE/c${SILENCE_KEYBOARD}" ${PATCH}
+    SILENCE_KEYBOARD="+     if ((type == UP_DEVICE_KIND_MOUSE || type == UP_DEVICE_KIND_KEYBOARD) && state == UP_DEVICE_STATE_DISCHARGING) {"
+    sed -i "/UP_DEVICE_KIND_MOUSE/c${SILENCE_KEYBOARD}" ${PATCH}
 fi
 patch -F 2 < ${PATCH}
 cd ..
 
-# Compile upowerd
-if [ "${USE_MESON}" == "false" ]; then
+# Compile upowerd according to the build system
+if [ "${BUILD_SYSTEM}" == "cmake" ]; then
     ./autogen.sh
     ./configure
     make
-elif [ "${USE_MESON}" == "true" ]; then
+elif [ "${BUILD_SYSTEM}" == "meson" ]; then
     meson _build -Dintrospection=enabled -Dman=true -Dgtk-doc=true -Didevice=enabled
     ninja -C _build
 else
-    echo "Invalid state; please open an issue on GitHub."
+    echo "Invalid build system"
+    unknown_system
     exit 1
 fi
 
 # Install upowerd
 CUR_DATETIME=`date +%Y-%m-%d-%H%M%S`
 pushd . # we're in ./silent-mouse/upower/
-if [ "${USE_MESON}" == "true" ]; then
+if [ "${BUILD_SYSTEM}" == "meson" ]; then
     cd _build/src
-else
+else # cmake
     cd src/.libs
 fi
 strip upowerd
@@ -179,9 +173,9 @@ popd
 
 # Install upower
 pushd .
-if [ "${USE_MESON}" == "true" ]; then
+if [ "${BUILD_SYSTEM}" == "meson" ]; then
     cd _build/tools
-else
+else # cmake
     cd tools/.libs
 fi
 strip upower
